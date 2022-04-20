8399143c3ad8b65c78d4b0df0b60b3f0d350c0a9
Instructor <instructor@techlab.training>
2022-04-20 21:30:39 +0100

    criado componente experimental

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -19,24 +19,34 @@ export default function Detail() {
     }));
   }
 
-  return (
-    <>
-    {stock &&(
-      <section>
-        <h2>{stock.name}</h2>
-        <h3>{stock.code}</h3>
-        <div>
-          Preço: {stock.price}€
-        </div>
-        <div>
-          <button onClick={toggleFavorite}>
-            {stock.isFavorite ? "Remover " : "Marcar "}
-            Favorito
-          </button>
-        </div>
-      </section>
-    )}
-    </>
+  function Display(){
+    if(stock)  {
+      if(stock.code){
+        return (
+          <section>
+          <h2>{stock.name}</h2>
+          <h3>{stock.code}</h3>
+          <div>
+            Preço: {stock.price}€
+          </div>
+          <div>
+            <button onClick={toggleFavorite}>
+              {stock.isFavorite ? "Remover " : "Marcar "}
+              Favorito
+            </button>
+          </div>
+        </section>
+        )
+      }
+      else{
+        return(<p>Stock inexistente</p>);
+      }
+    }
+    else{
+      return(<p>Aguarde um momento</p>);
+    }
+  }
+  return(
+   <Display/>
   )
-
 }
