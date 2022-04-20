6b74139a0b9e815ed1b7d80a537db865f2f22816
Instructor <instructor@techlab.training>
2022-04-20 20:06:48 +0100

    Foi adicionado state para submeter novos stocks

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Create() {
+  const [formData, setFormData] = useState({
+    name: "",
+    code: "",
+    price: 0
+  })
   function handleChange(event) {
     console.log("handleChange:", event.target.value);
+    const {name,value} = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,19 +25,22 @@ export default function Create() {
           <div>
             <label>
               nome
-              <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange}/>
+              <input type="text" name="name" required minLength="4" maxLength="60" 
+              onChange={handleChange} value={formData.name}/>
             </label>
           </div>
           <div>
             <label>
               Código
-              <input type="text" name="code" required minLength="3" maxLength="3" onChange={handleChange}/>
+              <input type="text" name="code" required minLength="3" maxLength="3" 
+              onChange={handleChange}  value={formData.code}/>
             </label>
           </div>
           <div>
           <label>
               Preço
-              <input type="number" name="price" required min="1" max="999999999" onChange={handleChange}/>
+              <input type="number" name="price" required min="1" max="999999999" 
+              onChange={handleChange} value={formData.price}/>
             </label>
           </div>
           <div>
